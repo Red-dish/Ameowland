8f004346a54a969a5890fe21046f015838e68373
Red-dish <reddhishri.monami@gmail.com>
2025-05-31 13:50:29 +0000

    Merging from dev

--- src/server-directory.js
+++ src/server-directory.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 export const serverDirectory = path.dirname(import.meta.dirname ?? path.dirname(fileURLToPath(import.meta.url)));
-=======
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-export const serverDirectory = path.dirname(import.meta.dirname ?? path.dirname(fileURLToPath(import.meta.url)));
->>>>>>> 9cabee9a8bd6934762b56d10f8e71ddb324410f2
