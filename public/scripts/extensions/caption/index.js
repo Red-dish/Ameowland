38a26ba18c21eda7c7acfe61ca4846e2d4c85f59
Red-dish <reddhishri.monami@gmail.com>
2025-05-31 13:40:33 +0000

    merging with dev

--- public/scripts/extensions/caption/index.js
+++ public/scripts/extensions/caption/index.js
@@ -664,3 +664,4 @@ jQuery(async function () {
 
     document.body.classList.add('caption');
 });
+
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 import { CommandLineParser } from './src/command-line.js';
 import { serverDirectory } from './src/server-directory.js';
@@ -14,20 +13,3 @@ try {
 } catch (error) {
     console.error('A critical error has occurred while starting the server:', error);
 }
-=======
-#!/usr/bin/env node
-import { CommandLineParser } from './src/command-line.js';
-import { serverDirectory } from './src/server-directory.js';
-
-// config.yaml will be set when parsing command line arguments
-const cliArgs = new CommandLineParser().parse(process.argv);
-globalThis.DATA_ROOT = cliArgs.dataRoot;
-globalThis.COMMAND_LINE_ARGS = cliArgs;
-process.chdir(serverDirectory);
-
-try {
-    await import('./src/server-main.js');
-} catch (error) {
-    console.error('A critical error has occurred while starting the server:', error);
-}
->>>>>>> 9cabee9a8bd6934762b56d10f8e71ddb324410f2
