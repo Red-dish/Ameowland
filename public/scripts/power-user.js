da97dc009d75a51f84e492f4db31e1da0501d094
Red-dish <reddhishri.monami@gmail.com>
2026-03-15 13:46:58 +1100

    - Increase chatfile size in chat.js and index.js - Remove content param from power-user.js - Prompt Inspection = False

--- public/scripts/power-user.js
+++ public/scripts/power-user.js
@@ -2306,7 +2306,7 @@ function validateStoryString(storyString, params) {
                 fieldsToWarn.push(field);
                 currentCache.fieldsWarned[field] = true;
             }
-            console.warn(`The story string does not contain {{${field}}}, but it would contain content:\n`, params[field]);
+            console.warn(`The story string does not contain {{${field}}}, but it would contain content:\n`);
         }
     }
 
